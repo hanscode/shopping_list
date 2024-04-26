154b3ce9561643a5701976c09909e167c9639f44
Hans Steffens <hans@steffens.co>
2024-04-26 11:37:14 -0500

    #4 User is able to add item to the list

--- shopping_list.py
+++ shopping_list.py
@@ -1,3 +1,11 @@
+# create a new empty list named shopping_list
+shopping_list = []
+
+# Create a function named add_to_list that declares a parameter named item
+def add_to_list(item):
+    # Add the item to the list
+    shopping_list.append(item)
+
 def show_help():
     print("What should we pick up at the store?")
     print("""
@@ -15,3 +23,6 @@ while True:
     elif new_item == 'HELP':
         show_help()
         continue
+
+    # Call add_to_list with new_item as an argument
+    add_to_list(new_item)
