35157a357784271d51d610c5b3fca11f022c7e49
Hans Steffens <hans@steffens.co>
2024-04-26 12:19:35 -0500

    #6 SHOW the list at any time and when the user #3 is DONE

--- shopping_list.py
+++ shopping_list.py
@@ -1,13 +1,6 @@
 # create a new empty list named shopping_list
 shopping_list = []
 
-# Create a function named add_to_list that declares a parameter named item
-def add_to_list(item):
-    # Add the item to the list
-    shopping_list.append(item)
-    # Notify user that the item was added, and state the number of items in the list currently
-    print("Added! List has {} items.".format(len(shopping_list)))
-
 def show_help():
     print("What should we pick up at the store?")
     print("""
@@ -15,6 +8,20 @@ Enter 'DONE' to stop adding items.
 Enter 'HELP' for this help.
 Enter 'SHOW' to see your current list.
 """)
+
+# Create a function named add_to_list that declares a parameter named item
+def add_to_list(item):
+    # Add the item to the list
+    shopping_list.append(item)
+    # Notify user that the item was added, and state the number of items in the list currently
+    print("Added! List has {} items.".format(len(shopping_list)))
+
+# Define a function named show_list that prints all the items in the list
+def show_list():
+    print("Here's your list:")
+    for item in shopping_list:
+        print(item)
+
 
 show_help()
 while True:
@@ -25,6 +32,11 @@ while True:
     elif new_item == 'HELP':
         show_help()
         continue
+    elif new_item == 'SHOW':
+        show_list()
+        continue
 
     # Call add_to_list with new_item as an argument
     add_to_list(new_item)
+
+show_list()
